cdbb4adaf0ba9fd4ff55339dbe7c7b424cd4a4f1
Christian Benincasa <chrisbenincasa@gmail.com>
2012-08-17 12:20:27 -0400

    add Apache license

--- glide.js
+++ glide.js
@@ -1,8 +1,26 @@
+/*
+
+  Copyright 2012 Christian Benincasa
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  * jQuery glide v0.9
+  * By: Christian Benincasa
+  * A simple, lightweight, multi-oriented, multi-directional jQuery carousel!
+
+*/
+
 ;(function($, window, document, undefined){
-  /**
-    * jQuery glide v0.1.1
-    * By: Christian Benincasa
-  **/
   $.fn.glide = function(options)
   {
     // If argument is one of the methods, call that methods with additional arguments
